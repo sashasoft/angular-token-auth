f7c78900096243f3f2f67955713f526c6d0de331
sashasoft <kovalenko.alexandrs@gmail.com>
2017-12-03 09:52:15 +0200

    first tets commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterializeModule } from 'angular2-materialize';
+import { AppComponent } from './app.component';
+import { Angular2TokenService } from 'angular2-token';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    AuthDialogComponent,
+    LoginFormComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    MaterializeModule,
+  ],
+  providers: [Angular2TokenService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
